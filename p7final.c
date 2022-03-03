ed7489ec0751d372cf628de2f057e5ede36f95d2
kush46 <97089328+kushal46@users.noreply.github.com>
2022-03-03 09:16:43 +0000

    1

--- p7final.c
+++ p7final.c
@@ -17,9 +17,30 @@ typedef struct _fraction Fraction;
   return a;
   
 }
+Fraction input_fraction()
+{
+  Fraction f;
+  printf("input a fraction\n");
+  scanf("%d%d",&f.num,&f.den);
+  return f;
+}
+
+Fraction add_fraction(Fraction f1,Fraction f2)
+{
+  Fraction sum;
+  sum.num = f1.num*f2.den+f2.num*f1.den;
+  sum.den = f1.den*f2.den;
+  int g = gcd(sum.num,sum.den);
+  sum.num=sum.num/g;
+  sum.den=sum.den/g;
+  return sum;
+}
 int main()
 {
-  printf("%d\n",gcd(16,24));
- return 0;
-  }
-
+  Fraction f1,f2,sum;
+  f1 = input_fraction();
+  f2 = input_fraction();
+  sum = add_fraction(f1,f2);
+  printf("%d/%d + %d/%d = %d/%d",f1.num,f1.den,f2.num,f2.den,sum.num,sum.den);
+  return 0;
+}
